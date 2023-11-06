8b2b6304939d3235d4e287ec8c9a555d502067a8
chukky01 <85424159+chukky01@users.noreply.github.com>
2023-11-06 10:42:28 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def twoSum(self, nums: List[int], target: int) ->List[int]:
+  #using a dictionary
+  seen ={}
+  for i, num in enumerate(nums):
+    if target - num in seen:
+      return ([seen[target - num], i])
+    elif num not in seen:
+      seen[num] = i
